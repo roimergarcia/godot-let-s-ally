96eb407d7e21e9feaa53ddc5bef0396ae5bd2ea5
Roimer Garcia <roimergarcia@gmail.com>
2023-09-24 17:18:07 -0400

    Added player's group and fixed sprite position

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-enum FASE_DIRECTION { UP, DOWN, LEFT, RIGHT }
+enum FACE_DIRECTION { UP, DOWN, LEFT, RIGHT }
 
 @export var speed = 200
 @export var rotation_speed = 5
 
 var walk_direction := Vector2(0, -1)
-var face_dir : FASE_DIRECTION = FASE_DIRECTION.DOWN; 
+var face_dir : FACE_DIRECTION = FACE_DIRECTION.DOWN; 
 
 func get_input():
 #	rotation_direction = Input.get_axis("left", "right")
@@ -14,31 +14,30 @@ func get_input():
  
 	#Walk Animation
 	if Input.is_action_pressed("up"):
-		face_dir = FASE_DIRECTION.UP
+		face_dir = FACE_DIRECTION.UP
 		$player_anim_2d.play("walk_up")
 	elif Input.is_action_pressed("down"):
-		face_dir = FASE_DIRECTION.DOWN
+		face_dir = FACE_DIRECTION.DOWN
 		$player_anim_2d.play("walk_down")
 	elif Input.is_action_pressed("left"):
-		face_dir = FASE_DIRECTION.LEFT
+		face_dir = FACE_DIRECTION.LEFT
 		$player_anim_2d.play("walk_left")
 	elif Input.is_action_pressed("right"):
-		face_dir = FASE_DIRECTION.RIGHT
+		face_dir = FACE_DIRECTION.RIGHT
 		$player_anim_2d.play("walk_right")
 	#Idle Animation
 	else:
 		match(face_dir):
-			FASE_DIRECTION.UP:
+			FACE_DIRECTION.UP:
 				$player_anim_2d.play("idle_up")
-			FASE_DIRECTION.DOWN:
+			FACE_DIRECTION.DOWN:
 				$player_anim_2d.play("idle_down")
-			FASE_DIRECTION.LEFT:
+			FACE_DIRECTION.LEFT:
 				$player_anim_2d.play("idle_left")
-			FASE_DIRECTION.RIGHT:
+			FACE_DIRECTION.RIGHT:
 				$player_anim_2d.play("idle_right")
-
+	
 	var input_direction := Input.get_vector("left", "right", "up", "down")
-	#print_debug(input_direction)
 	velocity = input_direction * speed
 
 func _physics_process(delta):
