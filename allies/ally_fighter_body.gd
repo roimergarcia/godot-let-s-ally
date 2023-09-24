4ea4d04f44e153bd412397433384df3dd6aaf401
Roimer Garcia <roimergarcia@gmail.com>
2023-09-24 18:30:23 -0400

    Corrección de player y ally_fighter: ahora no están en una sescena

--- allies/ally_fighter_body.gd
+++ allies/ally_fighter_body.gd
@@ -8,12 +8,17 @@ enum FACE_DIRECTION { UP, DOWN, LEFT, RIGHT }
 var walk_direction := Vector2(0, -1)
 var face_dir : FACE_DIRECTION = FACE_DIRECTION.DOWN; 
 
+var random_gen = RandomNumberGenerator.new()
+
 var player : CharacterBody2D = null
 var is_following_player := false
 
 func apply_move(delta):
 	if not is_following_player:
-	
+		if random_gen.randf() > .8:
+			#walk_direction = walk_direction.rotated() * random_gen.randf_range(0, 360)
+			pass
+		
 	
 	pass
 
