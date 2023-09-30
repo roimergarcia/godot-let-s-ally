fc7aae2c24caff865ac91a98ed28d3a285e5332c
Jhofranto <jhofrantovar94@gmail.com>
2023-09-30 19:11:08 -0400

    Adición de estados al enemigo (Falta corregir bug en la selección).
    Adición de estados al enemigo (Falta corregir bug en la selección).

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,40 +1,37 @@
 extends CharacterBody2D
 
 enum FASE_DIRECTION { UP, DOWN, LEFT, RIGHT }
+enum STATUS { CHASING, ATTACKING, WALKING, WAITING }
+
+const ENEMY_ATTACK = preload("res://enemy_attack/enemy_attack.tscn")
 
 @export var speed = 120
 @export var healt = 100
 
-const ATTACK = preload("res://enemy_attack/enemy_attack.tscn")
+var face_dir : FASE_DIRECTION = FASE_DIRECTION.DOWN
+var enemy_status : STATUS = STATUS.WAITING
 
 var player = null
-var player_chase = false
-var player_inattack_zone = false
 var player_last_position = Vector2(0,0)
 var can_take_damage = true
+var attacked = false
 
-var walking = false
-var attacking = false
-var face_dir : FASE_DIRECTION = FASE_DIRECTION.DOWN; 
-
-func chase_player():
-	if player_chase and not player_inattack_zone:
-		var angulo = position.angle_to_point (player.position)
-		walking = true
-		#print(position.dot((player.position - position).normalized()))
-		#print(position.angle_to_point (player.position))
+func chase_survivor():
+	if enemy_status == STATUS.CHASING:
+		var angle = position.angle_to_point (player.position)
 		velocity = position.direction_to(player.position) * speed
-		if angulo > -0.75 and angulo < 0.75:
+		player_last_position = player.position
+		if angle > -0.75 and angle < 0.75:
 			face_dir = FASE_DIRECTION.RIGHT
 			$enemy_anim_2d.play("walk_right")
 			$attack_area/enemy_attack_area_coll.position.x = 7
 			$attack_area/enemy_attack_area_coll.position.y = 0
-		elif angulo > 0.75 and angulo < 2.25:
+		elif angle > 0.75 and angle < 2.25:
 			face_dir = FASE_DIRECTION.DOWN
 			$enemy_anim_2d.play("walk_front")
 			$attack_area/enemy_attack_area_coll.position.x = 0
 			$attack_area/enemy_attack_area_coll.position.y = 7
-		elif angulo < -0.75 and angulo > -2.25:
+		elif angle < -0.75 and angle > -2.25:
 			face_dir = FASE_DIRECTION.UP
 			$enemy_anim_2d.play("walk_back")
 			$attack_area/enemy_attack_area_coll.position.x = 0
@@ -44,107 +41,103 @@ func chase_player():
 			$enemy_anim_2d.play("walk_left")
 			$attack_area/enemy_attack_area_coll.position.x = -7
 			$attack_area/enemy_attack_area_coll.position.y = 0
-	else:
-		if not walking and not attacking:
-			match(face_dir):
-				FASE_DIRECTION.UP:
-					$enemy_anim_2d.play("idle_back")
-				FASE_DIRECTION.DOWN:
-					$enemy_anim_2d.play("idle_front")
-				FASE_DIRECTION.LEFT:
-					$enemy_anim_2d.play("idle_left")
-				FASE_DIRECTION.RIGHT:
-					$enemy_anim_2d.play("idle_right")
+
+func wait_survivor():
+	var random_generator = RandomNumberGenerator.new()
+	var spin_chance = random_generator.randf_range(1, 100)
+	if enemy_status == STATUS.WAITING:
+		if spin_chance <= 2:
+			face_dir = FASE_DIRECTION.values()[randi() % FASE_DIRECTION.size()]
+		velocity = Vector2.ZERO
+		match(face_dir):
+			FASE_DIRECTION.UP:
+				$enemy_anim_2d.play("idle_back")
+			FASE_DIRECTION.DOWN:
+				$enemy_anim_2d.play("idle_front")
+			FASE_DIRECTION.LEFT:
+				$enemy_anim_2d.play("idle_left")
+			FASE_DIRECTION.RIGHT:
+				$enemy_anim_2d.play("idle_right")
 
 func random_movement():
 	var random_generator = RandomNumberGenerator.new()
-	var spin_chance = random_generator.randf_range(1, 100)
 	var walk_chance = random_generator.randf_range(1, 100)
-	var _face_directions = ["UP","DOWN","LEFT","RIGHT"]
-	
-	if not player_chase and not walking and not attacking and spin_chance <= 2:
-		face_dir = FASE_DIRECTION[_face_directions[randi() % _face_directions.size()]]
-		
-		if walk_chance > 98:
-			var divisor_velocidad = random_generator.randf_range(0.5, 4.0)
-			var walk_time = random_generator.randf_range(0.5, 4.0)
-			$walking.wait_time = walk_time
-			$walking.start()
-			walking = true
-			match(face_dir):
-				FASE_DIRECTION.UP:
-					velocity = Vector2.UP * (speed / divisor_velocidad)
-					$enemy_anim_2d.play("walk_back")
-				FASE_DIRECTION.DOWN:
-					velocity = Vector2.DOWN * (speed / divisor_velocidad)
-					$enemy_anim_2d.play("walk_front")
-				FASE_DIRECTION.LEFT:
-					velocity = Vector2.LEFT * (speed / divisor_velocidad)
-					$enemy_anim_2d.play("walk_left")
-				FASE_DIRECTION.RIGHT:
-					velocity = Vector2.RIGHT * (speed / divisor_velocidad)
-					$enemy_anim_2d.play("walk_right")
+	if enemy_status == STATUS.WAITING and walk_chance < 2:
+		var speed_divider = random_generator.randf_range(1, 4.0)
+		var walk_time = random_generator.randf_range(1, 4.0)
+		enemy_status = STATUS.WALKING
+		$walking.wait_time = walk_time
+		$walking.start()
+		match(face_dir):
+			FASE_DIRECTION.UP:
+				velocity = Vector2.UP * (speed / speed_divider)
+				$enemy_anim_2d.play("walk_back")
+			FASE_DIRECTION.DOWN:
+				velocity = Vector2.DOWN * (speed / speed_divider)
+				$enemy_anim_2d.play("walk_front")
+			FASE_DIRECTION.LEFT:
+				velocity = Vector2.LEFT * (speed / speed_divider)
+				$enemy_anim_2d.play("walk_left")
+			FASE_DIRECTION.RIGHT:
+				velocity = Vector2.RIGHT * (speed / speed_divider)
+				$enemy_anim_2d.play("walk_right")
 
-func attack():
-	if player_inattack_zone and not attacking:
+func attack_survivor():
+	if enemy_status == STATUS.ATTACKING and not attacked:
 		$attack_cooldown.start()
-		attacking = true
-		if not walking:
-			match(face_dir):
-				FASE_DIRECTION.UP:
-					$enemy_anim_2d.play("attack_back")
-				FASE_DIRECTION.DOWN:
-					$enemy_anim_2d.play("attack_front")
-				FASE_DIRECTION.LEFT:
-					$enemy_anim_2d.play("attack_left")
-				FASE_DIRECTION.RIGHT:
-					$enemy_anim_2d.play("attack_right")
-		var attack = ATTACK.instantiate()
+		attacked = true
+		match(face_dir):
+			FASE_DIRECTION.UP:
+				$enemy_anim_2d.play("attack_back")
+			FASE_DIRECTION.DOWN:
+				$enemy_anim_2d.play("attack_front")
+			FASE_DIRECTION.LEFT:
+				$enemy_anim_2d.play("attack_left")
+			FASE_DIRECTION.RIGHT:
+				$enemy_anim_2d.play("attack_right")
+		var attack = ENEMY_ATTACK.instantiate()
 		get_parent().add_child(attack)
 		attack.position = player.position
 
 func _physics_process(delta):
-	chase_player()
+	chase_survivor()
+	wait_survivor()
 	random_movement()
-	attack()
+	attack_survivor()
 	move_and_slide()
 
 
 func _on_detection_area_body_entered(body):
 	if body.is_in_group("Player"):
 		player = body
-		player_chase = true
+		enemy_status = STATUS.CHASING
 
 func _on_detection_area_body_exited(body):
 	var random_generator = RandomNumberGenerator.new()
-	var divisor_velocidad = random_generator.randf_range(1.5, 4.0)
+	var speed_divider = random_generator.randf_range(1.5, 4.0)
 	if body.is_in_group("Player"):
-		player_last_position = player.position
-		$continue_chasing.wait_time = divisor_velocidad
+		$continue_chasing.wait_time = speed_divider
 		$continue_chasing.start()
-		velocity = position.direction_to(player_last_position) * (speed / divisor_velocidad)
+		velocity = position.direction_to(player_last_position) * (speed / speed_divider)
 		player = null
-		player_chase = false
+		enemy_status = STATUS.WALKING
 
 func _on_attack_area_body_entered(body):
 	if body.is_in_group("Player"):
-		player_inattack_zone = true
+		enemy_status = STATUS.ATTACKING
 
 func _on_attack_area_body_exited(body):
 	if body.is_in_group("Player"):
-		player_inattack_zone = false
-
+		enemy_status = STATUS.CHASING
 
 func _on_continue_chasing_timeout():
-	velocity = position.direction_to(player_last_position) * 0
-	walking = false
-
+	velocity = Vector2.ZERO
+	enemy_status = STATUS.WAITING
 
 func _on_walking_timeout():
 	velocity = Vector2.ZERO
-	walking = false
-
+	enemy_status = STATUS.WAITING
 
 func _on_attack_cooldown_timeout():
-	attacking = false
+	attacked = false
 
--- enemy_attack/enemy_attack.gd
+++ enemy_attack/enemy_attack.gd
@@ -6,10 +6,8 @@ func _ready():
 func _on_attack_area_body_entered(body):
 	$attack_anim_2d.visible = true
 	$attack_anim_2d.play("attack")
-#	self.queue_free()
 	print(body.name)
 
 
 func _on_attack_anim_2d_animation_finished():
-	print("terminó")
 	self.queue_free()
