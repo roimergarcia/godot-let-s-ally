cfaae251a95a0feaa325d56416bdbecacb6aab95
Roimer Garcia <roimergarcia@gmail.com>
2023-10-02 21:41:00 -0400

    Movimiento de sobreviviente-fighter

--- allies/survivor_fighter.gd
+++ allies/survivor_fighter.gd
@@ -4,14 +4,15 @@ extends CharacterBody2D
 @export var STEERING_SPEED := 5
 @export var FOLLOW_RADIUS := 100
 
-const SAFE_PLAYER_DISTANCE := 200
+const SAFE_PLAYER_DISTANCE := 150
 
 enum FACE_DIRECTION { UP, DOWN, LEFT, RIGHT }
 enum STATE { IDLE, RANDOM_WALK, FOLLOWING, FIGHT_WALK, FIGHTING }
-var state : STATE = STATE.FOLLOWING
+var state : STATE = STATE.IDLE
 var face_dir := FACE_DIRECTION.DOWN
   
 @export var player : CharacterBody2D = null
+
 var walking = false
 var follow_player = false
 var fighting = false
@@ -26,46 +27,64 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func walk_follow_player():
-	if follow_player and not fighting:
-		var distance = position.distance_to(player.position)
+	if state == STATE.FOLLOWING:
+		var player_position = player.position
+		var distance = position.distance_to(player_position)
 		if (distance <= SAFE_PLAYER_DISTANCE): 
+			print("distance: %.02f" % distance)
 			return
-		var face_angle = position.angle_to_point(player.position) * 180 / PI
+			
+		var face_angle = position.angle_to_point(player_position) * 180 / PI
 		walking = true
 		
-		velocity = position.direction_to(player.position)* SPEED
+		velocity = position.direction_to(player_position) * SPEED
 		if face_angle >= 45 and face_angle <= 45:
 			face_dir = FACE_DIRECTION.RIGHT
 			$ally_fighter_anim_2d.play("walk_fighter_right")
 		elif face_angle > 45 and face_angle <= 135:
 			face_dir = FACE_DIRECTION.DOWN
-			$ally_fighter_anim_2d.play("walk_fighter_front")
+			$ally_fighter_anim_2d.play("walk_fighter_down")
 		elif face_angle < 45 and face_angle >= -135:
 			face_dir = FACE_DIRECTION.UP
-			$ally_fighter_anim_2d.play("walk_fighter_back")
+			$ally_fighter_anim_2d.play("walk_fighter_up")
 		else:
 			face_dir = FACE_DIRECTION.LEFT
 			$ally_fighter_anim_2d.play("walk_fighter_left")
-	else:
-		if not walking:
-			match(face_dir):
-				FACE_DIRECTION.UP:
-					$ally_fighter_anim_2d.play("idle_fighter_down")
-				FACE_DIRECTION.DOWN:
-					$ally_fighter_anim_2d.play("idle_fighter_up")
-				FACE_DIRECTION.LEFT:
-					$ally_fighter_anim_2d.play("idle_fighter_left")
-				FACE_DIRECTION.RIGHT:
-					$ally_fighter_anim_2d.play("idle_fighter_right")
+	elif(state == STATE.IDLE or state == STATE.FIGHTING):
+		match(face_dir):
+			FACE_DIRECTION.UP:
+				$ally_fighter_anim_2d.play("idle_fighter_down")
+			FACE_DIRECTION.DOWN:
+				$ally_fighter_anim_2d.play("idle_fighter_up")
+			FACE_DIRECTION.LEFT:
+				$ally_fighter_anim_2d.play("idle_fighter_left")
+			FACE_DIRECTION.RIGHT:
+				$ally_fighter_anim_2d.play("idle_fighter_right")
+
+
+func idle_walk():
+	if state != STATE.IDLE:
+		return 
+		
+	match(face_dir):
+		FACE_DIRECTION.UP:
+			$ally_fighter_anim_2d.play("idle_fighter_down")
+		FACE_DIRECTION.DOWN:
+			$ally_fighter_anim_2d.play("idle_fighter_up")
+		FACE_DIRECTION.LEFT:
+			$ally_fighter_anim_2d.play("idle_fighter_left")
+		FACE_DIRECTION.RIGHT:
+			$ally_fighter_anim_2d.play("idle_fighter_right")
 
 func random_movement():
+	if state != STATE.RANDOM_WALK:
+		return 
+		
 	var spin_chance = random_gen.randf_range(1, 100)
 	var walk_chance = random_gen.randf_range(1, 100)
-	var _face_directions = ["UP","DOWN","LEFT","RIGHT"]
 	
 	if not follow_player and not walking and spin_chance <= 2:
-		face_dir = FACE_DIRECTION[_face_directions[randi() % _face_directions.size()]]
-		print(face_dir)
+		face_dir = FACE_DIRECTION.values()[randi() % FACE_DIRECTION.size()]
 		
 		if walk_chance <= 98:
 			return 
@@ -92,25 +111,26 @@ func random_movement():
 func _on_survivor_fighter_detection_area_body_entered(body):
 	if body.is_in_group("Player"):
 		player = body
+		state = STATE.FOLLOWING
 		follow_player = true
 	
-func _on_survivor_fighter_detection_area_body_exited(body): 
-	var divisor_velocidad = random_gen.randf_range(1.5, 4.0)
-	if body.is_in_group("Player"):
-		player_last_position = player.position
-		$continue_following.wait_time = divisor_velocidad
-		$continue_following.start()
-		velocity = position.direction_to(player_last_position) * (SPEED / divisor_velocidad)
-		player = null
-		follow_player = false
+#func _on_survivor_fighter_detection_area_body_exited(body): 
+#	var divisor_velocidad = random_gen.randf_range(1.5, 4.0)
+#	if body.is_in_group("Player"):
+#		player_last_position = player.position
+#		$continue_following.wait_time = divisor_velocidad
+#		$continue_following.start()
+#		velocity = position.direction_to(player_last_position) * (SPEED / divisor_velocidad)
+#		player = null
+#		follow_player = false
 
 func _on_walking_timer_timeout():
 	velocity = Vector2.ZERO
 	walking = false
 
-func _on_continue_following_timeout():
-	velocity = position.direction_to(player_last_position) * 0
-	walking = false
+#func _on_continue_following_timeout():
+#	velocity = position.direction_to(player_last_position) * 0
+#	walking = false
 
  
 func move(target, delta):
